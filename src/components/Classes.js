be38f0277d398d44edc0d841a87e01628374b2c6
Brandon Salter <bsalter5444@gmail.com>
2024-12-09 09:31:57 -0500

    update

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -9,63 +9,63 @@ const classes = [
         id: 1,
         name: 'Scout',
         description: 'A fast and agile character who excels at hit-and-run tactics.',
-        image: 'https://wiki.teamfortress.com/w/images/0/0f/Scout_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/6/69/Scout.png',
         wikiLink: 'https://wiki.teamfortress.com/wiki/Scout'
     },
     {
         id: 2,
         name: 'Soldier',
         description: 'A versatile class that uses rocket launchers for high damage.',
-        image: 'https://wiki.teamfortress.com/w/images/4/4e/Soldier_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/7/7b/Soldier.png',
         wikiLink: 'https://wiki.teamfortress.com/wiki/Soldier'
     },
     {
         id: 3,
         name: 'Pyro',
         description: 'A close-range class that uses fire to deal damage.',
-        image: 'https://wiki.teamfortress.com/w/images/6/69/Pyro_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/c/c8/Pyro.png',
         wikiLink: 'https://wiki.teamfortress.com/wiki/Pyro'
     },
     {
         id: 4,
         name: 'Demoman',
         description: 'An explosive expert who uses grenades and sticky bombs.',
-        image: 'https://wiki.teamfortress.com/w/images/1/19/Demoman_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/f/fd/Demoman.png',
         wikiLink: 'https://wiki.teamfortress.com/wiki/Demoman'
     },
     {
         id: 5,
         name: 'Heavy',
         description: 'A tanky class with a powerful minigun.',
-        image: 'https://wiki.teamfortress.com/w/images/9/9b/Heavy_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/0/08/Heavy.png',
         wikiLink: 'https://wiki.teamfortress.com/wiki/Heavy'
     },
     {
         id: 6,
         name: 'Engineer',
         description: 'A supportive class that builds turrets and teleports.',
-        image: 'https://wiki.teamfortress.com/w/images/4/4f/Engineer_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/d/d8/Engineer.png',
         wikiLink: 'https://wiki.teamfortress.com/wiki/Engineer'
     },
     {
         id: 7,
         name: 'Medic',
         description: 'A healer who can revive and support teammates.',
-        image: 'https://wiki.teamfortress.com/w/images/1/17/Medic_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/2/26/Medic.png',
         wikiLink: 'https://wiki.teamfortress.com/wiki/Medic'
     },
     {
         id: 8,
         name: 'Sniper',
         description: 'A long-range marksman with a rifle.',
-        image: 'https://wiki.teamfortress.com/w/images/e/e2/Sniper_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/8/8f/Sniper.png',
         wikiLink: 'https://wiki.teamfortress.com/wiki/Sniper'
     },
     {
         id: 9,
         name: 'Spy',
         description: 'A stealthy class that can disguise as enemies.',
-        image: 'https://wiki.teamfortress.com/w/images/3/3e/Spy_icon.png',
+        image: 'https://wiki.teamfortress.com/w/images/3/36/Spy.png',
         wikiLink: 'https://wiki.teamfortress.com/w/images/3/3e/Spy'
     }
 ];
--- src/components/RandomWeapons.js
+++ src/components/RandomWeapons.js
@@ -8,38 +8,77 @@ const classWeapons = {
     Scout: [
         { name: 'Scattergun', image: 'https://wiki.teamfortress.com/w/images/1/1d/Scattergun_icon.png' },
         { name: 'Force-A-Nature', image: 'https://wiki.teamfortress.com/w/images/c/cd/Force-a-Nature_icon.png' },
+        { name: 'ShortStop', image: 'https://wiki.teamfortress.com/w/images/9/91/Shortstop.png' },
+        { name: 'Sodapopper', image: 'https://wiki.teamfortress.com/w/images/f/f1/Soda_Popper.PNG' },
+        { name: 'BabyFaceBlaster', image: 'https://wiki.teamfortress.com/w/images/thumb/b/b1/Babyfaceblaster.png/250px-Babyfaceblaster.png' },
+        { name: 'Back Scatter', image: 'https://wiki.teamfortress.com/w/images/0/0f/Back_Scatter.png' },
     ],
     Soldier: [
         { name: 'Rocket Launcher', image: 'https://wiki.teamfortress.com/w/images/7/7f/Rocket_Launcher_icon.png' },
         { name: 'Direct Hit', image: 'https://wiki.teamfortress.com/w/images/a/aa/Direct_Hit_icon.png' },
+        { name: 'Cow Mangler', image: 'https://wiki.teamfortress.com/w/images/2/2c/RED_Cow_Mangler_5000.png' },
+        { name: 'Black Box', image: 'https://wiki.teamfortress.com/w/images/4/41/Blackbox.png' },
+        { name: 'Rocket Jumper', image: 'https://wiki.teamfortress.com/w/images/d/d8/Rocket_Jumper.png' },
+        { name: 'Beggar Bazooka', image: 'https://wiki.teamfortress.com/w/images/5/50/Beggar%27s_Bazooka.png' },
+        { name: 'Air Strike', image: 'https://wiki.teamfortress.com/w/images/a/a2/Air_Strike.png' },
+        { name: 'Original', image: 'https://wiki.teamfortress.com/w/images/7/7f/Original.png' },
+        { name: 'Libetry Launcher', image: 'https://wiki.teamfortress.com/w/images/e/e2/Liberty_Launcher.png' }
+        
     ],
     Pyro: [
-        { name: 'Flamethrower', image: 'https://wiki.teamfortress.com/w/images/4/44/Flamethrower_icon.png' },
+        { name: 'Flamethrower/Reskins', image: 'https://wiki.teamfortress.com/w/images/4/44/Flamethrower_icon.png' },
         { name: 'Degreaser', image: 'https://wiki.teamfortress.com/w/images/6/68/Degreaser_icon.png' },
+        { name: 'BackBurner', image: 'https://wiki.teamfortress.com/w/images/f/f2/RedBackburnerTilt.png' },
+        { name: 'Phlog', image: 'https://wiki.teamfortress.com/w/images/7/77/RED_Phlogistinator.png' },
+        { name: 'Dragon Fury', image: 'https://wiki.teamfortress.com/w/images/9/94/RED_Dragon%27s_Fury.png' },
+        
     ],
     Demoman: [
         { name: 'Grenade Launcher', image: 'https://wiki.teamfortress.com/w/images/2/27/Grenade_Launcher_icon.png' },
-        { name: 'Stickybomb Launcher', image: 'https://wiki.teamfortress.com/w/images/4/4a/Stickybomb_Launcher_icon.png' },
+        { name: 'Loch n Load', image: 'https://wiki.teamfortress.com/w/images/4/48/LochnLoad.png' },
+        { name: 'Loose Cannon', image: 'https://wiki.teamfortress.com/w/images/3/36/Loose_Cannon.png' },
+        { name: 'Iron Bomber', image: 'https://wiki.teamfortress.com/w/images/5/54/Iron_Bomber.png' },
+        { name: 'Wee booties', image: 'https://wiki.teamfortress.com/w/images/5/53/Ali_Baba%27s_Wee_Booties.PNG' },
+        { name: 'BASE Jumper', image: 'https://wiki.teamfortress.com/w/images/0/06/B.A.S.E._Jumper.png' }
+        
     ],
     Heavy: [
         { name: 'Minigun', image: 'https://wiki.teamfortress.com/w/images/1/1d/Minigun_icon.png' },
         { name: 'Sasha', image: 'https://wiki.teamfortress.com/w/images/9/9d/Sasha_icon.png' },
+        { name: 'BrassBeast', image: 'https://wiki.teamfortress.com/w/images/3/38/Brass_Beast.png' },
+        { name: 'Tomislav', image: 'https://wiki.teamfortress.com/w/images/3/37/Tomislav.PNG' },
+        { name: 'Huo-Long Heater', image: 'https://wiki.teamfortress.com/w/images/3/3d/RED_Huo-Long_Heater.png' }
     ],
     Engineer: [
         { name: 'Wrench', image: 'https://wiki.teamfortress.com/w/images/d/d1/Wrench_icon.png' },
-        { name: 'Building Tools', image: 'https://wiki.teamfortress.com/w/images/a/ad/Building_Tools_icon.png' },
+        { name: 'GunSlinger', image: 'https://wiki.teamfortress.com/w/images/e/e7/RED_Gunslinger.png' },
+        { name: 'Jag', image: 'https://wiki.teamfortress.com/w/images/7/78/Jag.png' },
+        { name: 'Southern Hospitality', image: 'https://wiki.teamfortress.com/w/images/1/1f/Southern_Hospitality.png' },
+        { name: 'Eureka Effect', image: 'https://wiki.teamfortress.com/w/images/8/83/Eureka_Effect_RED.png' }
+        
     ],
     Medic: [
         { name: 'Medigun', image: 'https://wiki.teamfortress.com/w/images/7/7f/Medigun_icon.png' },
-        { name: 'Ubersaw', image: 'https://wiki.teamfortress.com/w/images/4/48/Ubersaw_icon.png' },
+        { name: 'Kritkrieg', image: 'https://wiki.teamfortress.com/w/images/1/19/RED_Kritzkrieg.png' },
+        { name: 'Quick-Fix', image: 'https://wiki.teamfortress.com/w/images/0/07/Quick-Fix_RED.png' },
+        { name: 'Vaccinator', image: 'https://wiki.teamfortress.com/w/images/7/72/RED_Vaccinator.png' },
     ],
     Sniper: [
-        { name: 'Sniper Rifle', image: 'https://wiki.teamfortress.com/w/images/e/e6/Sniper_Rifle_icon.png' },
+        { name: 'Sniper Rifle/reskin', image: 'https://wiki.teamfortress.com/w/images/e/e6/Sniper_Rifle_icon.png' },
         { name: 'Sydney Sleeper', image: 'https://wiki.teamfortress.com/w/images/e/eb/Sydney_Sleeper_icon.png' },
+        { name: 'Machina/reskin', image: 'https://wiki.teamfortress.com/w/images/b/bd/Machina.png' },
+        { name: 'Huntsman', image: 'https://wiki.teamfortress.com/w/images/7/75/Huntsman.png' },
+        { name: 'Bazaar Bargain', image: 'https://wiki.teamfortress.com/w/images/1/16/Bazaar_Bargain.PNG' },
+        { name: 'Classic', image: 'https://wiki.teamfortress.com/w/images/5/57/Classic.png' },
+        { name: 'Hitmans Heatmaker', image: 'https://wiki.teamfortress.com/w/images/0/0d/Hitman%27s_Heatmaker.png' },
     ],
     Spy: [
         { name: 'Butterfly Knife', image: 'https://wiki.teamfortress.com/w/images/8/8b/Butterfly_Knife_icon.png' },
-        { name: 'Revolver', image: 'https://wiki.teamfortress.com/w/images/7/7e/Revolver_icon.png' },
+        { name: 'Your Eternal Reward', image: 'https://wiki.teamfortress.com/w/images/d/db/RED_Eternal_Reward.png' },
+        { name: 'Kunai', image: 'https://wiki.teamfortress.com/w/images/2/24/Conniver%27s_Kunai.png' },
+        { name: 'Spy-cicle', image: 'https://wiki.teamfortress.com/w/images/9/9a/Spy-cicle.png' },
+        { name: 'Big Earner', image: 'https://wiki.teamfortress.com/w/images/b/be/Bigearner.PNG' },
+        
     ],
 };
 
