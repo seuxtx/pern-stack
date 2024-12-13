44a95d66960b9ff1944b14258c986376fd090561
Brandon Salter <bsalter5444@gmail.com>
2024-12-12 19:16:53 -0500

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div className="home-container">
-            <h1 className="home-title">Team Fortress 2 Loadouts</h1>
+            <h1 className="home-title">Team Fortress 2 Weapons</h1>
             <Classes />
             <Loadouts />
             <RandomWeapons /> {/* Add the RandomWeapons component here */}
